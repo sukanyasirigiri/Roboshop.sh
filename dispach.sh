f45853bd01de0e6e2a7ebdec721bbb6580a4ce66
sukanya <sukanyasirigiri@gmail.com>
2023-04-17 15:57:19 -0400

    frontendproject

---
+++ dispach.sh
@@ -0,0 +1,13 @@
+yum install golang -y
+useradd roboshop
+mkdir /app 
+curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip 
+cd /app 
+unzip /tmp/dispatch.zip
+go mod init dispatch
+go get 
+go build
+cp dispach.service /etc/systemd/system/dispatch.service
+systemctl daemon-reload
+systemctl enable dispatch 
+systemctl start dispatch
