a16ff90984182588b44c5bb1ae87814d36ab6a4a
sukanya <sukanyasirigiri@gmail.com>
2023-04-18 18:32:42 -0400

    user service

--- user.sh
+++ user.sh
@@ -1,15 +1,35 @@
+echo -e "\e[36m>>>>>>>> confifure nojs repos <<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo -e "\e[36m>>>>>>>> install nodejs <<<<<<<<<\e[0m"
 yum install nodejs -y
+
+echo -e "\e[36m>>>>>>>> add application user <<<<<<<<<\e[0m"
 useradd roboshop
+
+echo -e "\e[36m>>>>>>>> create application directory <<<<<<<<<\e[0m"
+rm -rf /app
 mkdir /app 
-curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip 
-cd /app 
+
+echo -e "\e[36m>>>>> download app content <<<<<<<<<\e[0m"
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
+cd /app  
+
+echo -e "\e[36m>>>>>>>> unzip app content <<<<<<<<<\e[0m"
 unzip /tmp/user.zip 
+
+echo -e "\e[36m>>>>>>>> install nodejs dependencies <<<<<<<<<\e[0m"
 npm install 
-cp user.service /etc/systemd/system/cuser.service
+
+echo -e "\e[36m>>>>>>>> create application directory <<<<<<<<<\e[0m"
+cp /home/centos/roboshop.sh/user.service /etc/systemd/system/cuser.service
+
+echo -e "\e[36m>>>>>>>> start user service <<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable user
-systemctl start user
+systemctl rstart user
+
+echo -e "\e[36m>>>>>>>> create application directory <<<<<<<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org-shell -y
 mongo --host mongodb.devops1722.com </app/schema/catalogue.js
