12318b129d9897c0817f820e45a85de26daabb17
sukanya <sukanyasirigiri@gmail.com>
2023-04-19 11:11:55 -0400

    cart service

--- cart.sh
+++ cart.sh
@@ -1,12 +1,30 @@
+echo -e "\e[36m>>>>>>>> configuring nodejs repos <<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo -e "\e[36m>>>>>>>> install nodejs <<<<<<<<<\e[0m"
 yum install nodejs -y
+
+echo -e "\e[36m>>>>>>>> add application user <<<<<<<<<\e[0m"
 useradd roboshop
+
+echo -e "\e[36m>>>>>>>> create application directory <<<<<<<<<\e[0m"
+rm -rf /app
 mkdir /app 
+
+echo -e "\e[36m>>>>>>>> download app content <<<<<<<<<\e[0m"
 curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip 
 cd /app 
+
+echo -e "\e[36m>>>>>>>> unzip app content <<<<<<<<<\e[0m"
 unzip /tmp/cart.zip 
-npm install 
-cp cart.service /etc/systemd/system/cart.service
+
+echo -e "\e[36m>>>>>>>> install nodejs dependencies <<<<<<<<<\e[0m"
+npm install
+
+echo -e "\e[36m>>>>>>>> copy cart systemd file <<<<<<<<<\e[0m"
+cp /home/centos/Roboshop.sh/cart.service /etc/systemd/system/cart.service
+
+echo -e "\e[36m>>>>>>>> start cart service <<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable cart
-systemctl start cart
+systemctl restart cart
