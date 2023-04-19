ed2655b3f26354dcd66688e95343040d5bb2403f
sukanya <sukanyasirigiri@gmail.com>
2023-04-19 18:21:14 -0400

    mysqlservice

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,15 @@
+echo -e "\e[36m>>>>>>>> disable mysql default version <<<<<<<<<\e[0m"
 dnf module disable mysql -y 
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+
+echo -e "\e[36m>>>>>>>> copy mysql repofile <<<<<<<<<\e[0m"
+cp /home/centos/Roboshop.sh/mysql.repo /etc/yum.repos.d/mysql.repo
+
+echo -e "\e[36m>>>>>>>> install mysql <<<<<<<<<\e[0m"
 yum install mysql-community-server -y
+
+echo -e "\e[36m>>>>>>>> start mysql <<<<<<<<<\e[0m"
 systemctl enable mysqld
-systemctl start mysqld  
+systemctl restart mysqld  
+
+echo -e "\e[36m>>>>>>>> reset mysql password <<<<<<<<<\e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
